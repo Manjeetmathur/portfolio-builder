1483fb733a664a621fa4fb527aec10e1c3b6a4e2
Manjeet Kumar <manjeetkumar62054@gmail.com>
2025-01-11 02:23:45 +0530

    n

--- portfolio/src/backendUrl/Backendurl.js
+++ portfolio/src/backendUrl/Backendurl.js
@@ -1,2 +1,2 @@
-export const url = 'https://portfolio-builder-backend-sable.vercel.app'
+export const url = 'https://portfolio-builder-o3sb.onrender.com'
 // export const url = 'http://localhost:8000'
