d9af889fef71a695c5d7886e1602dba9e6d658ab
Manjeet Kumar <manjeetkumar62054@gmail.com>
2025-01-12 04:11:16 +0530

    m

--- portfolio-backend/controllers/userController.js
+++ portfolio-backend/controllers/userController.js
@@ -73,7 +73,7 @@ const login = async (req, res) => {
     const options = {
       httpOnly: true,
       secure: true,
-      // sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 30 * 24 * 60 * 60 * 1000
     };
     return res.cookie("token", token, options).json({
